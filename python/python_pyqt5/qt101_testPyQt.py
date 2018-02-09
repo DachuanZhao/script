87b9962e23ba44c28d638623a2c30957bad4cd44
DachuanZhao <hnjyzdc@hotmail.com>
2018-02-09 14:00:03 +0800

    delete pyqt5

--- python/python_pyqt5/qt101_testPyQt.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5 import QtWidgets,QtCore
-
-app = QtWidgets.QApplication(sys.argv)
-widget = QtWidgets.QWidget()
-widget.resize(360,360)
-widget.setWindowTitle("hello pyqt5")
-widget.show()
-sys.exit(app.exec_())
--- python/python_pyqt5/qt102_PrintApi.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QWidget
-
-out = sys.stdout
-sys.stdout = open(r'D:\dropbox\Dropbox\git_hnjyzdc\git_python\python_pyqt5\QWidget.txt','w')
-help(QWidget)
-sys.stdout.close()
-sys.stdout = out
