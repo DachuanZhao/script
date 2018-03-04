c63d0c5970e81d7802853b80c890fad25c566fb2
DachuanZhao <hnjyzdc@hotmail.com>
2018-03-04 22:13:47 +0800

    test yxyArchivage.py

--- python/yxyArchivage.py
+++ python/yxyArchivage.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  3 17:06:19 2018
+
+@author: Administrator
+"""
+
 import urllib.request
 import datetime
 import time
@@ -5,16 +12,23 @@ import sys
 import os.path
 
 url="http://jadyn.strasbourg.eu/GPS/dynn.xml"
-count = 0
+count_e = 0
+count_s = 0
+file_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+time_wait = 5
 
 while True:
     try:
-        dt=datetime.datetime.now()
-        dt = dt.replace(microsecond=0)
-        urllib.request.urlretrieve(url, os.path."FILE_%(dt)s.xml" %{"dt":dt})
-        time.sleep(180)
+        dt=str(datetime.datetime.now().replace(microsecond=0)).replace(" ","_").replace(":","_")
+        output_path = os.path.join(file_path,("".join(("FILE_",str(dt),".xml"))))
+        print("this is ",count_s," TH success")
+        print("output_path is :",output_path)
+        count_s = count_s + 1
+        urllib.request.urlretrieve(url,output_path)
+        time.sleep(time_wait)
     except ConnectionResetError as e:
         print("error is ",e)
-        time.sleep(180)
-        count = count  + 1
-        print("this is ",count" TH error")
+        time.sleep(time_wait)
+        count_e = count_e  + 1
+        print("this is ",count_e," TH error")
+        
